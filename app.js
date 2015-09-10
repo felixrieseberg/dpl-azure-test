7c1848231878fb4ec387e9f2a75845dfa4a28345
Felix Rieseberg <felix@felixrieseberg.com>
2015-09-10 13:57:48 -0700

    Travis

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Travis!\n");
+  response.end("Hello Travis :)!\n");
 });
 
 server.listen(process.env.PORT || 80);
